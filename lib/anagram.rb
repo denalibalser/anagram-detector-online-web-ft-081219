fe65dc77d3e4dc6e31271b66ed96621e3a01c755
Denali Balser <denali.balser167@gmail.com>
2019-08-27 16:24:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,7 +2,7 @@ require 'pry'
 class Anagram
   attr_accessor :word
 
-  @@matches = []
+
   def initialize(word)
     @word = word
 
@@ -11,12 +11,8 @@ class Anagram
   def match(array)
     array.select do |word|
 
-      if @word.split(" ").sort == word.split(" ").sort
-binding.pry
-        @@matches << word
+       @word.split(" ").sort == word.split(" ").sort
       end
     end
-
   end
-    @@matches
 end
