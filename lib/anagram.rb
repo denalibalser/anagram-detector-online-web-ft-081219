6cd2814ad5b1c87af63112f885e51f528314124a
Denali Balser <denali.balser167@gmail.com>
2019-08-27 16:17:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,12 +3,13 @@ class Anagram
 
   def initialize(word)
     @word = word
+    @@matches = []
   end
 
   def match(array)
     array.select do |word|
       @word.split(" ").sort == word.split(" ").sort
     end
-      
+
   end
 end
